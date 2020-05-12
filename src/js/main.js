f36db2166c426dee3325d1bca36a760ecf911f12
hoàng vũ <hoangvm1799>
2020-05-12 17:37:58 +0700

    update

--- src/js/main.js
+++ src/js/main.js
@@ -444,7 +444,7 @@ jQuery(document).ready(function($){
 // ////////////gallery navigation end/////////////
 $(document).ready(function() {
     var page = window.location.pathname;
-	if(page == '/' || page == '/default.aspx'){
+	if($('body').hasClass('homepage')){
         $(".header__bottom .menu__list li:nth-child(2)").addClass('services-scroll');
     }else{
 		$(".header__bottom .menu__list li:nth-child(2)").addClass('modal-toggle');
